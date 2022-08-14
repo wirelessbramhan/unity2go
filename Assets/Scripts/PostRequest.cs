973337a3af50ea2ab6f22746058ca9e2a0f9ca4d
Sarbajit Mukhopadhyaya <szar.sarbajeet@gmail.com>
2022-08-14 18:45:32 +0530

    Update PostRequest.cs

--- Assets/Scripts/PostRequest.cs
+++ Assets/Scripts/PostRequest.cs
@@ -6,35 +6,23 @@ using UnityEngine.Networking;
 
 public class PostRequest : MonoBehaviour
 {
-    public Serializer Serializer;
-    public Deserializer Deserializer;
+    public Serializer Serializer; // serializes customer_state value on button click
+    public Deserializer Deserializer; // reads WebRequest Response
     
-    public string rawJSONData;
-    public string requestData;
+    public string rawJSONData; // deserialized json to pass to API USING Serializer
+    public string requestData; // serialized json string to create Response Objects
 
-    public AudioSource AudioSource;
+    public AudioSource AudioSource; // AudioSource to hold the audio Clip
 
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator Post(string url, string bodyJsonString) // Coroutine to Run WebRequest
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    IEnumerator Post(string url, string bodyJsonString)
-    {
-        var request = new UnityWebRequest(url, "POST");
+        var request = new UnityWebRequest(url, "POST"); // variables to hold request object and Raw Data to pass to API
         byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
         
-        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw); // explicit buffers to avoid errors
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer(); // as above
         
-        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Content-Type", "application/json"); // custom headers for authentication and content type
         request.SetRequestHeader("X-I2CE-ENTERPRISE-ID", "dave_expo");
         
         request.SetRequestHeader("X-I2CE-USER-ID", "74710c52-42a5-3e65-b1f0-2dc39ebe42c2");
@@ -46,13 +34,13 @@ public class PostRequest : MonoBehaviour
 
         Debug.Log(request.downloadHandler.text);
 
-        Deserializer.CreateFromJSON(request.downloadHandler.text);
+        Deserializer.CreateFromJSON(request.downloadHandler.text); // Deserialize Response into classes
         Debug.Log(Deserializer.placeholder);
         
-        //StartCoroutine(GetAudioClip());
+        //StartCoroutine(GetAudioClip()); // Audio Clip could not be Parsed accurately
     }
 
-    IEnumerator GetAudioClip()
+    IEnumerator GetAudioClip() // Coroutine to get Audio CLip after successful Call
     {
         using UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(Deserializer.response_channels.voice, AudioType.WAV);
         yield return www.SendWebRequest();
@@ -68,13 +56,13 @@ public class PostRequest : MonoBehaviour
         }
     }
 
-    public void PostData()
+    public void PostData() // Post Data button calls function on Click after serializing Raw Data to json
     {
         rawJSONData = Serializer.SaveToString();
         StartCoroutine(Post("https://test.iamdave.ai/conversation/exhibit_aldo/74710c52-42a5-3e65-b1f0-2dc39ebe42c2", rawJSONData));
     }
 
-    public void DisplayResponse()
+    public void DisplayResponse() // plays the result Clip
     {
         AudioSource.Play();
     }
