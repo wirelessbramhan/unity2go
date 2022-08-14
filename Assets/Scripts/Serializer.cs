0fff28b08a1754d524bcdeb9ad825e398a13345b
Sarbajit Mukhopadhyaya <szar.sarbajeet@gmail.com>
2022-08-14 18:35:43 +0530

    Update Serializer.cs
    added comments

--- Assets/Scripts/Serializer.cs
+++ Assets/Scripts/Serializer.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Serializer : MonoBehaviour
-{
+{  // Matched variables to json Raw Data to be be passed
     public string system_response;
     public string engagement_id;
     public string customer_state;
 
-    public string SaveToString()
+    public string SaveToString() // Converts to json string to be passed to PostData
     {
         return JsonUtility.ToJson(this);
     }
